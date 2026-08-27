06a64f55f7910f768df44b3ed5e6e4de34565c71
oshango <junioronema@hotmail.com>
2026-08-27 09:03:51 +0100

    Fix Swagger configuration so API builds correctly

--- api/KinTraffic.Api/Program.cs
+++ api/KinTraffic.Api/Program.cs
@@ -2,13 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
+app.MapOpenApi();
 app.MapControllers();
 
 app.Run();
